128cf67e9173281f9a2119b960bafa24d34e6ae6
Eazybel <eazybel27@gmail.com>
2023-10-05 17:44:09 +0300

    practicing

--- practice 1.c
+++ practice 1.c
@@ -1,37 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-#include <conio.h>
+#include<stdio.h>
 
 int main()
 {
-    FILE *file;
-    char choice;
-    char web[20];
-    printf("\t\t\tWELCOME TO Credential Manager\n");
-    printf("\t\t\t----------------------------------------\n");
-    printf("[1] Add new credential.\n[2] Delete existing credentials.\n[3] Search for a specific credential by Web name\n");
-    printf("... ");
-    choice=getch();
-switch (choice)
-{
-case '1':
-    printf("Enter Your Web Name: ");
-
-gets(web);
-file=fopen("cre.txt","w");
-fprintf(file,"%s",web);
-fprintf(file,"\n----------------------------------------",web);
-fclose(file);
-    break;
-case '2':
-    /* code */
-    break;
-case '3':
-    /* code */
-    break;
-default:
-    break;
-}
-
+    for (size_t i = 0; i < 8; i++)
+    {
+        printf("#\n");
+    }
     return 0;
 }
--- pyramid of asterics.c
+++ pyramid of asterics.c
@@ -1,20 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
+int main()
 
-int main() {
-    int numRows;
-
-    printf("Enter the number of rows for the pyramid: ");
-    scanf("%d", &numRows);
-
-    for (int i = 1; i <= numRows; i++) {
-        for (int j = 1; j <= numRows - i; j++) {
-            printf(" ");
-        }
-        for (int k = 1; k <= i; k++) {
-            printf("#");
-        }
-        printf("\n");
-    }
+{
+do
+{
+    
+} while ();
 
     return 0;
 }
