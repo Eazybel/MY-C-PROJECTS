72e7f4a8d8a82e3a54588b20cf712bc5f993107a
Eazybel <eazybel27@gmail.com>
2023-10-06 23:08:58 +0300

    cs 50 courses

--- practice 1.c
+++ practice 1.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
-
+#include<string.h>
+#include<ctype.h>
 int main()
 {
-    for (size_t i = 0; i < 8; i++)
+    int i;
+    int asci=65;
+    char name[6]={EZEDIN};
+    int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    for ( i = 0; i <= 25; i++)
     {
-        printf("#\n");
+        POINTS[i]=asci;
+        printf("%i\n",asci);
+        asci++;
     }
+    
+    
     return 0;
 }
