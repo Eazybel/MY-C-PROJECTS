58921ff998f7f76293e29787be66c2c142fc2d09
Eazybel <eazybel27@gmail.com>
2023-10-13 20:45:12 +0300

    The following changes were made:
    * The Visual Studio Code settings were updated to disable secure no warnings for C++.
    * practice 2.c was deleted.
    * practice 3.c was updated to add checks for uppercase, lowercase, digits, and symbols in a password.
    * practice.c was deleted. (generated by useblackbox.ai )

--- practice 3.c
+++ practice 3.c
@@ -1,11 +1,53 @@
 #include<stdio.h>
-void eazy(int x){
-    x=15;
-    printf("%i",x);
-    return ;
-}
+#include<string.h>
+#include<ctype.h>
+#include<stdbool.h>
 int main()
 {
-    eazy();
+    char password[9];
+    printf("Please enter your password: ");
+    gets(password);
+    int digit=0;
+    int upper=0;
+    int lower=0;
+    int symbol=0;
+   for (int i = 0; i < strlen(password); i++)
+   {
+       if (isdigit(password[i]))
+       {
+        ++digit;
+       }
+       else if (isupper(password[i]))
+       {
+        ++upper;
+       }
+       else if (islower(password[i]))
+       {
+        ++lower;
+       }
+       else if (!isalnum(password[i]))
+       {
+        ++symbol;
+       }
+       
+       
+   }
+   if (upper==0 || digit==0 || lower==0 || symbol==0)
+   {
+    printf("Your password needs at least one uppercase letter, lowercase letter, number and symbol!\n");
+   }
+
+if (upper!=0 && digit!=0 && lower!=0 && symbol!=0 && strlen(password) == 9)
+   {
+    printf("Successful\n");
+   }
+
+   if (strlen(password) != 9)
+   {
+    printf("Your password needs to be 8 characters long");
+   }
+   
+   
+   
     return 0;
 }
