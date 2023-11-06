4a1a8d056203e4e63ea023485de7111f5c5ab559
Eazybel <eazybel27@gmail.com>
2023-11-06 22:19:00 +0300

    The following changes were made:
    -   The working directory was changed from "c:/Users/Ezedin/MY-C-PROJECTS/MY-C-PROJECTS" to "c:/Users/Ezedin/MY-C-PROJECT/MY-C-PROJECTS".
    -   The program was changed from "c:/Users/Ezedin/MY-C-PROJECTS/MY-C-PROJECTS/build/Debug/outDebug" to "c:/Users/Ezedin/MY-C-PROJECT/MY-C-PROJECTS/build/Debug/outDebug".
    -   The following files were deleted: practice_2.exe, pyramid of asterics.exe, random number game.exe

---
+++ pointer addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+int num1;
+int num2;
+int *ad1=&num1;
+int *ad2=&num2;
+    printf("enter the number:\n");
+    scanf("%i",&num1);
+    printf("enter the number2:\n");
+    scanf("%i",&num2);
+
+    printf("your pointer sum=%i: ",*ad1+*ad2);
+    return 0;
+}
