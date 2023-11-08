69c38acc107b01ab287b543c4d0e678453eeeeaf
Eazybel <eazybel27@gmail.com>
2023-11-08 15:28:15 +0300

    Commit message:
    Windows gcc compiler changed from x64 to x86. String length of 'reverse' increased from 10 to 20. scanf now takes 'reverse' as an argument instead of '&reverse'. A comment was added and then removed.

--- string_reverser.c
+++ string_reverser.c
@@ -4,17 +4,17 @@
 int main()
 {
 
-char reverse[10];
-int *string=&reverse;
+char reverse[20];
+// int *string=&reverse;
 printf("enter your name: \n");
-scanf("%s",&reverse);
+scanf("%s",reverse);
 int last=strlen(reverse);
-// for (int i = last; i >= 0; i--)
-// { 
-//    printf("%c",reverse[i]);
+for (int i = last; i >= 0; i--)
+{ 
+   printf("%c",reverse[i]);
    
-// }
-printf("%s",*string);
+}
+
 
     return 0;
 }
